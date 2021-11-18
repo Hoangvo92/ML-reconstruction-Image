6947553119ad539383d46bd04ddf82e4c020e4a0
Hoang Vo <hoangvod@gmail.com>
2021-11-17 23:55:30 -0600

    working on tkinter gui

--- GUI_reconstruct_mri_tk_HV.py
+++ GUI_reconstruct_mri_tk_HV.py
@@ -7,63 +7,73 @@ import sys
 import math
 import random
 
+#GUI_reconstruct_mri_tk_HV.py
 
 
 
-
-'''
-    The Qt MainWindow class
-    A vtk widget and the ui controls will be added to this main window
-'''
-class MainWindow(tk.Frame):
-
-    def __init__(self, master = None):
-        super().__init__(master)
-        self.pack()
-         ''' Step 1: Initialize the Qt window '''
-        self.master.title("MRI Reconstruction- Hoang Vo")
-        #https://www.geeksforgeeks.org/open-a-new-window-with-a-button-in-python-tkinter/        
-        self.master.geometry("1000x500")
+#class MainWindow(tk.Frame):
+#
+ #   def __init__(self, master = None):
+  #      super().__init__(master)
+   #     self.pack()
+   #      ''' Step 1: Initialize the Qt window '''
+    #    self.master.title("MRI Reconstruction- Hoang Vo")
+     #   #https://www.geeksforgeeks.org/open-a-new-window-with-a-button-in-python-tkinter/
+    #    self.master.geometry("1000x500")
         
-        self.master.grid_rowconfigure(0, weight=1)
-        self.master.grid_columnconfigure(0, weight=1)
-        ''' Step 1: Initialize the Qt window '''
+     #   self.master.grid_rowconfigure(0, weight=1)
+      #  self.master.grid_columnconfigure(0, weight=1)
+       # ''' Step 1: Initialize the Qt window '''
  
         
 
         
-        ''' Step 3: Add the control panel to the right hand side of the central widget '''
-        # Note: To add a widget, we first need to create a widget, then set the layout for it
-        self.right_panel_widget = Qt.QWidget() # create a widget
-        self.right_panel_layout = Qt.QVBoxLayout() # set layout - lines up the controls vertically
-        self.right_panel_widget.setLayout(self.right_panel_layout) #assign the layout to the widget
-        self.mainLayout.addWidget(self.right_panel_widget) # now, add it to the central frame
-        
-        # The controls will be added here
-        self.add_controls()
+        #''' Step 3: Add the control panel to the right hand side of the central widget '''
+ 
                 
-        
+      #https://www.youtube.com/watch?v=Aim_7fC-inw
+      #https://www.tutorialsteacher.com/python/create-gui-using-tkinter-python
         
 master = tk.Tk()
 master.title("MRI Reconstruction- Hoang Vo")
 #app = MainWindow(master)
 master.geometry("1000x500")
-master.iconbigmap("ML-reconstruction-Image/")
+#master.iconbitgmap("ML-reconstruction-Image/")
 
 def open():
     global myImage
     
-    master.filename = filedialog.askopenfilename(initialdir="/OASIS1", title="Select A File", filetypes=(("jpg files", "*.jpg"),("all files", "*.*")))
+    master.filename = filedialog.askopenfilename(initialdir="data", title="Select A File", filetypes=(("png files", "*.png"),("all files", "*.*")))
                   
     myLabel = Label(master, text = master.filename).pack()
+    print(myLabel)
     #processing image in here
-    myImage = ImageTk.photoImage(Image.open(master.filename))
+   #https://stackoverflow.com/questions/52558118/how-to-display-multiple-images-inside-tkinter-window
+    myImage = ImageTk.PhotoImage(Image.open(master.filename))
                   
     myImageLabel = Label(image=MyImage).pack()
-    
 
+image1 = ImageTk.PhotoImage(Image.open("testing/test.png"))
+first_image = Label(image = image1 )
+first_image.grid(row=0, column=0, columnspan=4)
+second_image = Label(image = image1)
+second_image.grid(row=1, column=0, columnspan=4)
+third_image = Label(image = image1)
+third_image.grid(row=2, column=0, columnspan=4)
+                
                   
-btn =  Button(master, text= "Open File" , command = open).pack()
+btn_open =  Button(master, text= "Open File" , command = open) #.pack()
+
+btn_add_noise = Button(master, text ="Add Noise")
+btn_reconstruct = Button(master, text="Reconstruct Image")
+btn_parameters = Button(master, text="Predict Values")
+
+
+
+btn_open.grid(row = 3, column= 0)
+btn_add_noise.grid(row=3, column= 1)
+btn_reconstruct.grid(row=3, column=2)
+btn_reconsctruct.grid(row=3, column =3)
     
-master.maibloop()                 
+master.mainloop()
 #app.mainloop()
