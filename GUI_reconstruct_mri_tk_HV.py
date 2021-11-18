259841284b20ef55da2f3a706308708b99895036
Hoang Vo <hoangvod@gmail.com>
2021-11-18 01:51:13 -0600

    get some rough idea

--- GUI_reconstruct_mri_tk_HV.py
+++ GUI_reconstruct_mri_tk_HV.py
@@ -34,46 +34,88 @@ import random
       #https://www.youtube.com/watch?v=Aim_7fC-inw
       #https://www.tutorialsteacher.com/python/create-gui-using-tkinter-python
         
-master = tk.Tk()
+master = Tk()
 master.title("MRI Reconstruction- Hoang Vo")
 #app = MainWindow(master)
 master.geometry("1000x500")
 #master.iconbitgmap("ML-reconstruction-Image/")
 
 def open():
+    global first_image
     global myImage
     
+    #first_image.grid_forget()
+    
     master.filename = filedialog.askopenfilename(initialdir="data", title="Select A File", filetypes=(("png files", "*.png"),("all files", "*.*")))
-                  
-    myLabel = Label(master, text = master.filename).pack()
-    print(myLabel)
+    top = Toplevel()
+    top.title("Chosen Image")
+    myLabel = Label(master, text = master.filename)#.pack()
+    #print(myLabel)
     #processing image in here
    #https://stackoverflow.com/questions/52558118/how-to-display-multiple-images-inside-tkinter-window
-    myImage = ImageTk.PhotoImage(Image.open(master.filename))
-                  
-    myImageLabel = Label(image=MyImage).pack()
+    image = Image.open(master.filename)
+   # resized = myImage.resize((300,300),Image.ANTIALIAS)
+    myImage = ImageTk.PhotoImage(Image.open(master.filename))#.resize((300,300),Image.ANTIALIAS))
+    #tk_image = ImageTk.PhotoImage(myImage)
+           
+    first_image = Label(top, image= myImage)#.pack()
+    first_image.grid(row=0, column=0, columnspan=2)
+    #btn2 = Button(top, text="Add Noise").pack() #, command = top.destroy)
+    #btn2.grid(row=1, column= 0)
+    
+def noise_image():
+    global second_image
+    global noiseImage
+    
+    #first_image.grid_forget()
+    top = Toplevel()
+    top.title("Noise Image")
+    
+    image = Image.open("testing/test.png")
+    # do noise
+   # resized = myImage.resize((300,300),Image.ANTIALIAS)
+    noiseImage = ImageTk.PhotoImage(image)#.resize((300,300),Image.ANTIALIAS))
+    #tk_image = ImageTk.PhotoImage(myImage)
+           
+    second_image = Label(top, image= noiseImage)#.pack()
+    second_image.grid(row=0, column=0, columnspan=2)
 
-image1 = ImageTk.PhotoImage(Image.open("testing/test.png"))
-first_image = Label(image = image1 )
-first_image.grid(row=0, column=0, columnspan=4)
-second_image = Label(image = image1)
-second_image.grid(row=1, column=0, columnspan=4)
-third_image = Label(image = image1)
-third_image.grid(row=2, column=0, columnspan=4)
+#image1 = ImageTk.PhotoImage(Image.open("testing/test.png").resize((300,300),Image.ANTIALIAS))
+#first_image = Label(image = image1 )
+#first_image.grid(row=0, column=0, columnspan=2)
+#second_image = Label(image = image1)
+#second_image.grid(row=0, column=2, columnspan=2)
+#third_image = Label(image = image1)
+#third_image.grid(row=0, column=4, columnspan=2)
                 
                   
 btn_open =  Button(master, text= "Open File" , command = open) #.pack()
 
-btn_add_noise = Button(master, text ="Add Noise")
+btn_add_noise = Button(master, text ="Add Noise", command= noise_image)
 btn_reconstruct = Button(master, text="Reconstruct Image")
 btn_parameters = Button(master, text="Predict Values")
 
 
 
 btn_open.grid(row = 3, column= 0)
-btn_add_noise.grid(row=3, column= 1)
-btn_reconstruct.grid(row=3, column=2)
-btn_parameters.grid(row=3, column =3)
+btn_add_noise.grid(row=3, column= 2)
+btn_reconstruct.grid(row=3, column=4)
+btn_parameters.grid(row=3, column =6)
     
 master.mainloop()
 #app.mainloop()
+
+
+#for noise pop up image
+#top = Toplevel()
+#top.title("Noise Image")
+#noise_image = ImageTk.PhotoImage("noise image.png")
+#noise_label = Label(top, image= noise_image).pack()
+#btn2 = Button(top, text="Destroy", command = top.destroy)
+
+
+
+
+
+#https://www.youtube.com/watch?v=YXPyB4XeYLA
+#https://stackoverflow.com/questions/23584325/cannot-use-geometry-manager-pack-inside
